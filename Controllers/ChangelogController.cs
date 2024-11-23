cd0bed492820d5a6aa7f8d950a0f3fe0b9dfb6a5
Eaoi1234 <jestapol.suks@mail.kmutt.ac.th>
2024-11-23 16:12:28 +0700

    CRUD function

---
+++ Controllers/ChangelogController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameDrop.Data;
+using Microsoft.EntityFrameworkCore;
+using GameDrop.Models;
+
+namespace GameDrop.Controllers
+{
+    public class ChangelogController : Controller
+    {
+        private readonly GameDropDBContext _db;
+
+        public ChangelogController(GameDropDBContext db)
+        {
+            _db = db;
+        }
+        // GET: Admin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductDescription,ProductPrice,ProductImage")] GameDrop_Product products)
+        {
+            if (id != products.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (products.ProductImage != null && products.ProductImage.Length > 0)
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            products.ProductImage.CopyTo(memoryStream);
+                            products.ProductImageData = memoryStream.ToArray();
+                            products.ProductImageType = products.ProductImage.ContentType;
+                        }
+                    }
+                    _db.Update(products);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+
+
+        // GET: Admin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _db.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _db.Products.FindAsync(id);
+            if (products != null)
+            {
+                _db.Products.Remove(products);
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _db.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
+
