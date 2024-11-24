8f184780453a2b7bb77062187bf7d5a3838e449a
HugeBloom <20868603+HugeKC01@users.noreply.github.com>
2024-11-25 00:11:31 +0700

    Add SearchController and search functionalty

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -12,6 +12,24 @@ namespace GameDrop.Controllers
             _db = db;
         }
 
-        
+        public IActionResult Index(string SearchItem)
+        {
+            if (string.IsNullOrEmpty(SearchItem))
+            {
+                return View("Index", _db.Products.ToList());
+            }
+            else
+            {
+                var searchResults = _db.Products.Where(ProductSearch =>
+                    ProductSearch.ProductId.ToString().Contains(SearchItem) ||
+                    (ProductSearch.ProductName != null && ProductSearch.ProductName.Contains(SearchItem)) ||
+                    (ProductSearch.ProductDescription != null && ProductSearch.ProductDescription.Contains(SearchItem))
+                ).ToList();
+
+                return View("Index", searchResults);
+            }
+        }
+
+
     }
 }
