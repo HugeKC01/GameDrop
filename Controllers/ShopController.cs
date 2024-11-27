728bbdca355b7e53ad70a02a9ac62f8a01b9a4a0
HugeBloom <20868603+HugeKC01@users.noreply.github.com>
2024-11-27 09:39:21 +0700

    Add support for customer-side details page in shop controller and update README.md

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GameDrop.Data;
+using GameDrop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameDrop.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly GameDropDBContext _context;
+
+        public ShopController(GameDropDBContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,36 @@
 Our final project of CMM349 Web Application Development in King Mongkut’s University of Technology Thonburi.
 <br>Create the E-commerce website for Game and Gaming Gadget using Asp.NET core MVC with Bootstrap 5.3
 
+<h1>Features Checklist:</h1>
+
+<h3>System:</h3>
+
+- ✅ Product Database
+- ❎ Order System
+- ❎ Payment System
+- ❎ Coupon System
+- ❎ Shopping Cart
+- ❎ Category System
+- ✅ Search System
+- ❎ Review System
+- ❎ Rating System
+- ❎ Filter System
+- ❎ Receipt System
+- ❎ User Roles
+- ✅ User Registration
+- ✅ User Login
+
+<h3>Pages:</h3>
+
+- ❎ Home Page
+- ❎ Product Page
+- ❎ Product Detail Page
+- ❎ Cart Page
+- ❎ Checkout Page
+- ❎ Payment Page
+- ❎ User Profile
+...More to be added
+
 <br>
 <br>
 This project will be set to Public after Initial Release on Azure Cloud.
