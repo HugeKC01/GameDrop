07c806a24056ca4416efb904918ad46e24ea2494
HugeBloom <20868603+HugeKC01@users.noreply.github.com>
2024-11-27 19:14:07 +0700

    Add Details product view for customer side

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showFullscreenImage(imgSrc) {
+    var overlay = document.getElementById('fullscreenOverlay');
+    var img = document.getElementById('fullscreenImage');
+    img.src = imgSrc;
+    overlay.classList.add('show');
+}
+
+function hideFullscreenImage() {
+    var overlay = document.getElementById('fullscreenOverlay');
+    overlay.classList.remove('show');
+}
