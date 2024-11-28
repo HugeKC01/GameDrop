b4c8dc2791ec70bbc1302e6f815c15ec88c1782d
HugeBloom <20868603+HugeKC01@users.noreply.github.com>
2024-11-28 16:08:34 +0700

    Add UserAddress model

---
+++ Models/GameDrop_UserAddress.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameDrop.Models
+{
+    public class GameDrop_UserAddress
+    {
+        [Key]
+        [Required]
+        public int UserAddressId { get; set; }
+        [Required]
+        public string? UserAddress { get; set; }
+        [Required]
+        public string? UserCity { get; set; }
+        [Required]
+        public string? UserState { get; set; }
+        [Required]
+        public string? UserZip { get; set; }
+        [Required]
+        public string? UserCountry { get; set; }
+        [Required]
+        public string? UserPhone { get; set; }
+        [Required]
+        public string? UserEmail { get; set; }
+        public string? UserId { get; set; }
+    }
+}
