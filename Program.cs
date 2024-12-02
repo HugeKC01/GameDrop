d3499314e25174078e2c72682e7ef640ed955f30
HugeBloom <20868603+HugeKC01@users.noreply.github.com>
2024-12-02 16:17:05 +0700

    fix Program.cs

--- Program.cs
+++ Program.cs
@@ -41,3 +41,24 @@ builder.Services.Configure<IdentityOptions>(options =>
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
